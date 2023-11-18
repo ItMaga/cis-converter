e3e932c51a72b13f8518fa2abf58a15611fbe255
ItMaga <m-chemurziev@ya.ru>
2023-11-18 20:34:47 +0300

    feat: start layout

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
+  <main class="app">
+    <header class="app__header">
+      <RouterLink to="/">
+        <h1>CI's converter</h1>
+      </RouterLink>
+      <RouterLink to="/">Main</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <a
+        href="https://github.com/ItMaga/cis-converter"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        GitHub
+      </a>
+    </header>
+
     <RouterView />
-  </div>
+  </main>
 </template>
+
+<style scoped>
+.app__header {
+  display: grid;
+  grid-template-columns: auto 1fr auto auto;
+  column-gap: 8px;
+  align-items: center;
+  justify-items: flex-end;
+}
+</style>
